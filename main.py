0de7ef9b02ac035738f7d3dda558d30de4db1d4f
abhyamgupta123 <abhyamgupta123@gmail.com>
2021-01-01 20:39:03 +0530

    main file to intialise everything

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+# os.environ['STATIC'] = os.path.join(os.getcwd(), 'Requip', 'static')
+# os.environ['FILES'] = os.path.join(os.getcwd(), 'Requip', 'media')
+from dotenv import load_dotenv
+load_dotenv()
+from Contacts_project import app
+from waitress import serve
+serve(app, host="0.0.0.0", port=8080)
+""" uncomment this below line, and comment out the above line to use this project in debug/development mode """
+# app.run(debug=True, host="0.0.0.0")
